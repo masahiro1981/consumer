216280d6f6be0b38b3665419418186f8f40dff94
Masahiro Nakagawa <masahiro.nakagawa@canonical.com>
2022-11-25 13:01:50 +0000

    Initial commit

---
+++ src/provider.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+$SNAP_DATA/bsp-bin/bin/readfile
+
+#cat $SNAP_DATA/bsp-bin/data/sample.conf
+
+# writing to read only filesystem?
+#echo "Adding line from Consumer snap" > $SNAP_DATA/bsp-bin/data/sample.conf
+
